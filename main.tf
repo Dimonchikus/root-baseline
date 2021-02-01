0d02870658bd654a20ac02f08379f39f3289c180
Dimonchikus <dimonchikakus@gmail.com>
2021-02-01 16:47:42 +0200

    S3 Bucket added

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 variable "bucket_name" {
   type = string
   description = "Bucket name"
-  default = "Test Bucket_name"
+  default = "Test Bucket name"
 }
 
 resource "aws_s3_bucket" "test-bucket" {
